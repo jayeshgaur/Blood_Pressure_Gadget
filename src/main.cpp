84643e97687d895b5de3ce06a44e134740b6917e
Jayesh Gaur <jayesh.gaur@somaiya.edu>
2022-05-13 18:15:14 -0400

    Boiler plate code to get all readings and calculate systolic value

--- src/main.cpp
+++ src/main.cpp
@@ -35,8 +35,9 @@ DigitalOut slave_slct(PA_4);
 #define output_min 419430.4
 #define output_max 3774873.6
 
-float raw_pressure[1000] = {0};
-float previous_pressure = 0.0;
+float pressure_values[1000] = {0};
+float differences[1000] = {0};
+
 float pressure = 0.0;
 int i = 0;
 
@@ -46,7 +47,7 @@ void config_spi()
   spi.frequency(500000);
 }
 
-void get_pressure()
+float get_pressure()
 {
   slave_slct = 1;
 
@@ -73,33 +74,93 @@ void get_pressure()
 
   u_int32_t output1 = byte1 << 16 | byte2 << 8 | byte3;
 
-  previous_pressure = pressure;
-
   // Transfer Function B
-  pressure = (((output1 - output_min) * (300)) / (output_max - output_min));
+  return (((output1 - output_min) * (300)) / (output_max - output_min));
 
-  if((previous_pressure - pressure) > 1.5){
-    printf("You are releasing the pressure too fast, please slow down");
-  }
-  else if((previous_pressure - pressure) < 0.5){
-    printf("You are releasing the pressure too slow, please release the pressure faster");
-  }
-  else{
-    printf("Deflation rate is perfect!");
-  }
-  printf("\nOutput: : %0.2lf", pressure);
-  raw_pressure[i] = pressure;
-  i = i + 1;
+  // printf("\n Difference: %0.2lf", pressure - previous_pressure);
+
+  // if((previous_pressure - pressure) < -1.5){
+  //   printf("You are releasing the pressure too fast, please slow down");
+  // }
+  // else if((previous_pressure - pressure) > -0.5){
+  //   printf("You are releasing the pressure too slow, please release the pressure faster");
+  // }
+  // else{
+  //   printf("Deflation rate is perfect!");
+  // }
+  // printf("\nOutput: : %0.2lf", pressure);
+  // pressure_values[i] = pressure;
+  // i = i + 1;
   // printf("RD2: %d", byte2);
   // printf("RD3: %d", byte3);
-  wait_us(500000);
+  // wait_us(200000);
 }
 
 int main()
 {
+  float temp = 0;
+  float current_pressure = 0.0;
+  float previous_pressure = 0.0;
+  float difference = 0.0;
   config_spi();
-  while(i<500){
-    get_pressure();
+  while (1)
+  {
+    // Increasing pressure
+    while (current_pressure < 200)
+    {
+      printf("\nCurrent pressure is: %f \n. Please get the pressure to 200mm/Hg to start the processing.", current_pressure);
+      current_pressure = get_pressure();
+      wait_us(500000);
+    }
+
+    //Maximum pressure reached, now start deflation
+    i = 0;
+    previous_pressure = current_pressure;
+
+    // Decreasing pressure
+    while (1)
+    {
+      current_pressure = get_pressure();
+      
+      pressure_values[i] = current_pressure;
+      printf("\nCurrent pressure is: %0.2lf \n. Please maintain 1.5mm/Hg reduction per second.", current_pressure);
+      wait_us(333333);
+
+      difference = current_pressure - previous_pressure;
+      printf("\nDifference from previous pressure is: %0.2lf. Please maintain 1.5mm/Hg reduction per second.", difference);
+      if(current_pressure < 30){
+        break;
+      }
+      differences[i] = difference;
+      printf("\nAA %0.2lf. AA", differences[i]);
+      if ((difference) < -1)
+      {
+        printf("\nYou are releasing the pressure too fast, please slow down");
+      }
+      else if ((difference) > -0.5)
+      {
+        printf("\nYou are releasing the pressure too slow, please release the pressure faster");
+      }
+      else
+      {
+        printf("\nDeflation rate is perfect!");
+      }
+      previous_pressure = current_pressure;
+      i++;
+    }
+
+    printf("START OF CALCULATIONS");
+    //Calculations
+    //int length_of_pressure_array = sizeof(pressure_values)/sizeof(pressure_values[0]);
+    //int length_of_differences = sizeof(differences)/sizeof(differences[0]);
+
+    i=0;
+    temp = differences[i];
+    while(temp < 0){
+      i++;
+      temp = differences[i];
+    }
+    printf("Systolic Pressure: %0.2lf", differences[i]);
+    return 0;
   }
-  return 0;
 }
